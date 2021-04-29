8a8a70973467887b3f340ff1820fe405781cce90
RustemB <rustemb@systemli.org>
2021-04-29 14:54:07 +0500

    also check, if it has non *-bin version in aur

--- list-officialized-bin-pkgs.sh
+++ list-officialized-bin-pkgs.sh
@@ -3,6 +3,6 @@
 CECM="core extra community multilib"
 
 # *-bin detector
-(pacman -Sl chaotic-aur | awk '{print $2}' | grep -- '-bin$' | sed 's/-bin$//'; pacman -Sl $CECM | awk '{print $2}') | sort | uniq -d | awk '{print $1"-bin"}'
+(pacman -Sl chaotic-aur | awk '{print $2}' | grep -- '-bin$' | sed 's/-bin$//'; pacman -Sl $CECM chaotic-aur | awk '{print $2}') | sort | uniq -d | awk '{print $1"-bin"}'
 # aur -> cecm moved
 pacman -Sl $CECM chaotic-aur | awk '{print $2}' | sort | uniq -d
