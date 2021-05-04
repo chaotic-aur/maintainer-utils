d34579630aafa499f45c941f0ffdd19b43d69d77
RustemB <rustemb@systemli.org>
2021-05-04 20:34:17 +0500

    fixed

--- list-officialized-bin-pkgs.sh
+++ list-officialized-bin-pkgs.sh
@@ -3,6 +3,6 @@
 CECM="core extra community multilib"
 
 # *-bin detector
-(pacman -Sql chaotic-aur | grep -- '-bin$' | sed 's/-bin$//'; pacman -Sql $CECM chaotic-aur | sort | uniq -d | awk '{print $1"-bin"}'
+(pacman -Sql chaotic-aur | grep -- '-bin$' | sed 's/-bin$//'; pacman -Sql $CECM chaotic-aur) | sort | uniq -d | awk '{print $1"-bin"}'
 # aur -> cecm moved
 pacman -Sql $CECM chaotic-aur | sort | uniq -d
