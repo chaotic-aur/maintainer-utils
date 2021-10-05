5fda5e9329442ff32bfe612747b2d2454ad6932c
RustemB <rustemb@systemli.org>
2021-10-06 02:27:45 +0500

    second version of script to check *-bin packages

---
+++ list-officialized-bin-pkgs-v2.sh
@@ -0,0 +1,13 @@
+#!/bin/env sh
+
+REPOS="core extra community multilib chaotic-aur"
+
+# shellcheck disable=SC2086
+{
+	# *-bin detector
+	BINRX="(^$(pacman -Sql chaotic-aur | rg -r '' -- '-bin$' | sd '\n' '$|^')@)"
+	BINRX="$(echo $BINRX | sd -s '|^@' '')"
+	pacman -Sql $REPOS | rg -o --color never -- "$BINRX"
+	# aur -> cecm moved
+	pacman -Sql $REPOS | sort | uniq -d
+}
